e9d4de6c72002548841770f6c5b9d2ef20ad8e5f
MATHEUS LUIZ KUNZ <matheus.kunz@acad.univates>
2025-05-05 19:47:40 -0300

    Configurado Cliente.DAO

--- src/entidades/DAO/ClienteDAO.java
+++ src/entidades/DAO/ClienteDAO.java
@@ -4,7 +4,11 @@
  */
 package entidades.DAO;
 
+import apoio.ConexaoBD;
 import entidades.Cliente;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 /**
  *
@@ -12,7 +16,46 @@ import entidades.Cliente;
  */
 public class ClienteDAO {
     
-    public void salvar(Cliente c){
-        String sql= "";
+     ResultSet resultadoQ = null;
+
+    public void salvar(Cliente c) throws SQLException {
+        String sql = ""
+                + "INSERT INTO cliente (nome, cpf, endereco, data_nasc) VALUES ("
+                + "'" + c.getNome() + "',"
+                + "'" + c.getCpf() + "',"
+                + "'" + c.getTelefone() + "',"
+                + "'" + c.getEmail() + "',"
+                + "'" + c.getDataNascimento() + "'"
+                + ")";
+
+        System.out.println("sql: " + sql);
+
+        ConexaoBD.executeUpdate(sql);
     }
+
+    public ArrayList<Cliente> recuperarTodos() throws SQLException {
+        ArrayList<Cliente> clientes = new ArrayList();
+
+        String sql = ""
+                + "SELECT * FROM cliente ";
+
+        resultadoQ = ConexaoBD.executeQuery(sql);
+
+        while (resultadoQ.next()) {
+            Cliente cliente = new Cliente();
+
+            cliente.setId(resultadoQ.getInt("id"));
+            cliente.setNome(resultadoQ.getString("nome"));
+            cliente.setCpf(resultadoQ.getString("cpf"));
+            cliente.setTelefone(resultadoQ.getString("telefone"));
+            cliente.setEmail(resultadoQ.getString("email"));
+            cliente.setDataNascimento(resultadoQ.getString("data_nascimento"));
+
+            clientes.add(cliente);
+        }
+
+        return clientes;
+    
+    }
+
 }
