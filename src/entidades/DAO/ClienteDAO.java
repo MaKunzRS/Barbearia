3ae00dfa976508843bf1d3b7d13b6ba98311c1c3
MaKunzRS <kunzmatheus2001@gmail.com>
2025-05-06 20:28:26 -0300

    Update ClienteDAO.java

--- src/entidades/DAO/ClienteDAO.java
+++ src/entidades/DAO/ClienteDAO.java
@@ -20,7 +20,7 @@ public class ClienteDAO {
 
     public void salvar(Cliente c) throws SQLException {
         String sql = ""
-                + "INSERT INTO cliente (nome, cpf, telefone, email, dataNascimento) VALUES ("
+                + "INSERT INTO cliente (nome, cpf, telefone, email, data_nasc) VALUES ("
                 + "'" + c.getNome() + "',"
                 + "'" + c.getCpf() + "',"
                 + "'" + c.getTelefone() + "',"
@@ -49,7 +49,7 @@ public class ClienteDAO {
             cliente.setCpf(resultadoQ.getString("cpf"));
             cliente.setTelefone(resultadoQ.getString("telefone"));
             cliente.setEmail(resultadoQ.getString("email"));
-            cliente.setDataNascimento(resultadoQ.getString("dataNascimento"));
+            cliente.setDataNascimento(resultadoQ.getString("data_nasc"));
 
             clientes.add(cliente);
         }
@@ -73,7 +73,7 @@ public class ClienteDAO {
             cliente.setTelefone(resultadoQ.getString("telefone"));
             cliente.setCpf(resultadoQ.getString("cpf"));
             cliente.setEmail(resultadoQ.getString("email"));
-            cliente.setDataNascimento(resultadoQ.getString("dataNascimento"));
+            cliente.setDataNascimento(resultadoQ.getString("data_nasc"));
         }
         return cliente;
     }
